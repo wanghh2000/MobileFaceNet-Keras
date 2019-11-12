9ad815f9ada12714d41d62977975420732e25045
TMaysGGS <tmaysggs@gmail.com>
2019-11-12 18:55:30 +0800

    Update MobileFaceNet_save_model_for_prediction.py

--- MobileFaceNet_save_model_for_prediction.py
+++ MobileFaceNet_save_model_for_prediction.py
@@ -5,18 +5,16 @@ Created on Mon May 13 14:39:29 2019
 @author: TMaysGGS
 """
 
-'''Last updated on 10/28/2019 09:09'''
+'''Last updated on 11/12/2019 16:26'''
 '''Importing the libraries'''
-import math
-import tensorflow as tf
 from keras import backend as K
 from keras.models import Model
 from keras.layers import BatchNormalization, Conv2D, PReLU, Input, SeparableConv2D, DepthwiseConv2D, add, Flatten, Dense, Dropout
-from keras.engine.topology import Layer
-from keras import initializers
 from keras.utils import plot_model
 
-NUM_LABELS = 26928
+from Tools.Keras_custom_layers import ArcFaceLossLayer 
+
+NUM_LABELS = 67960
 
 '''Building Block Functions'''
 def conv_block(inputs, filters, kernel_size, strides, padding):
@@ -25,7 +23,7 @@ def conv_block(inputs, filters, kernel_size, strides, padding):
     
     Z = Conv2D(filters, kernel_size, strides = strides, padding = padding, use_bias = False)(inputs)
     Z = BatchNormalization(axis = channel_axis)(Z)
-    A = PReLU()(Z)
+    A = PReLU(shared_axes = [1, 2])(Z)
     
     return A
 
@@ -35,7 +33,7 @@ def separable_conv_block(inputs, filters, kernel_size, strides):
     
     Z = SeparableConv2D(filters, kernel_size, strides = strides, padding = "same", use_bias = False)(inputs)
     Z = BatchNormalization(axis = channel_axis)(Z)
-    A = PReLU()(Z)
+    A = PReLU(shared_axes = [1, 2])(Z)
     
     return A
 
@@ -48,7 +46,7 @@ def bottleneck(inputs, filters, kernel, t, s, r = False):
     
     Z1 = DepthwiseConv2D(kernel, strides = 1, padding = "same", depth_multiplier = 1, use_bias = False)(Z1)
     Z1 = BatchNormalization(axis = channel_axis)(Z1)
-    A1 = PReLU()(Z1)
+    A1 = PReLU(shared_axes = [1, 2])(Z1)
     
     Z2 = Conv2D(filters, 1, strides = 1, padding = "same", use_bias = False)(A1)
     Z2 = BatchNormalization(axis = channel_axis)(Z2)
@@ -75,78 +73,6 @@ def linear_GD_conv_block(inputs, kernel_size, strides):
     Z = BatchNormalization(axis = channel_axis)(Z)
     
     return Z
-
-# Arc Face Loss Layer (Class)
-class ArcFaceLossLayer(Layer):
-    '''
-    Arguments:
-        inputs: the input embedding vectors
-        class_num: number of classes
-        s: scaler value (default as 64)
-        m: the margin value (default as 0.5)
-    Returns:
-        the final calculated outputs
-    '''
-    def __init__(self, class_num, s = 64., m = 0.5, **kwargs):
-        
-        self.init = initializers.get('glorot_uniform') # Xavier uniform intializer
-        self.class_num = class_num
-        self.s = s
-        self.m = m
-        super(ArcFaceLossLayer, self).__init__(**kwargs)
-        
-    def build(self, input_shape):
-        
-        assert len(input_shape[0]) == 2 and len(input_shape[1]) == 2
-        self.W = self.add_weight((input_shape[0][-1], self.class_num), initializer = self.init, name = '{}_W'.format(self.name))
-        super(ArcFaceLossLayer, self).build(input_shape)
-        
-    def call(self, inputs, mask = None):
-        
-        cos_m = math.cos(self.m)
-        sin_m = math.sin(self.m)
-        mm = sin_m * self.m
-        threshold = math.cos(math.pi - self.m)
-        
-        # features
-        X = inputs[0] 
-        # 1-D or one-hot label works as mask
-        Y_mask = inputs[1] 
-        # If Y_mask is not in one-hot form, transfer it to one-hot form.
-        if Y_mask.shape[-1] == 1: 
-            Y_mask = K.cast(Y_mask, tf.int32)
-            Y_mask = K.reshape(K.one_hot(Y_mask, self.class_num), (-1, self.class_num))
-        
-        X_normed = K.l2_normalize(X, axis = 1) # L2 Normalized X
-        self.W = K.l2_normalize(self.W, axis = 0) # L2 Normalized Weights
-        
-        # cos(theta + m)
-        cos_theta = K.dot(X_normed, self.W)
-        cos_theta2 = K.square(cos_theta)
-        sin_theta2 = 1. - cos_theta2
-        sin_theta = K.sqrt(sin_theta2 + K.epsilon())
-        cos_tm = self.s * ((cos_theta * cos_m) - (sin_theta * sin_m))
-        
-        # This condition controls the theta + m should in range [0, pi]
-        #   0 <= theta + m < = pi
-        #   -m <= theta <= pi - m
-        cond_v = cos_theta - threshold
-        cond = K.cast(K.relu(cond_v), dtype = tf.bool)
-        keep_val = self.s * (cos_theta - mm)
-        cos_tm_temp = tf.where(cond, cos_tm, keep_val)
-        
-        # mask by label
-        Y_mask =+ K.epsilon()
-        inv_mask = 1. - Y_mask
-        s_cos_theta = self.s * cos_theta
-        
-        output = K.softmax((s_cos_theta * inv_mask) + (cos_tm_temp * Y_mask))
-        
-        return output
-    
-    def compute_output_shape(self, input_shape):
-        
-        return input_shape[0], self.class_num 
 
 '''Building the MobileFaceNet Model'''
 def mobile_face_net():
