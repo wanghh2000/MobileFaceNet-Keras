01a6ae7942fbbe7533f17ae3cfdf74c89d301832
TMaysGGS <tmaysggs@gmail.com>
2019-11-25 09:58:47 +0800

    Update training scripts

--- Training/MobileFaceNet_resume_train.py
+++ Training/MobileFaceNet_resume_train.py
@@ -1,29 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 25 10:58:15 2019
+Created on Mon May 13 16:54:33 2019
 
 @author: TMaysGGS
 """
 
-'''Last updated on 11/12/2019 15:47'''
-'''Importing the libraries'''
-import os 
+'''Updated on 11/25/2019 09:52'''
+'''Problem
+There is an intermediate layer that has the shape (m, 128), which means a 26928 * 128 matrix. 
+Each element takes 32 bit so that the total space needed is more than 21G, which way too much exceeds 
+the GPU memory. 
+'''
+'''Loading the pre-trained model'''
+import os
 import sys 
 import keras 
 from keras import backend as K
 from keras.models import Model
-from keras.layers import Input, Conv2D, BatchNormalization, PReLU, SeparableConv2D, DepthwiseConv2D, add, Flatten, Dense, Dropout
+from keras.layers import BatchNormalization, Conv2D, PReLU, Input, SeparableConv2D, DepthwiseConv2D, add, Flatten, Dense, Dropout
 from keras.optimizers import Adam
+# import keras.backend.tensorflow_backend as KTF
+# from keras.utils import plot_model
 
 sys.path.append('../') 
 from Tools.Keras_custom_layers import ArcFaceLossLayer 
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '3' # 如需多张卡设置为：'1, 2, 3'，使用CPU设置为：''
+'''Set if the GPU memory needs to be restricted
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.85
+session = tf.Session(config = config)
 
-BATCH_SIZE = 128
-NUM_LABELS = 67960 
-m = 15090270
+KTF.set_session(session)
+'''
+BATCH_SIZE = 128 
+old_m = 15090270
+m = 15090270 
 DATA_SPLIT = 0.005
+OLD_NUM_LABELS = 67960
+NUM_LABELS = 67960 
+TOTAL_EPOCHS = 1000 
+IMG_DIR = '/data/daiwei/processed_data/datasets_for_face_recognition' 
 
 '''Importing the data set'''
 from keras.preprocessing.image import ImageDataGenerator
@@ -138,9 +155,9 @@ def mobile_face_net():
     M = Dropout(rate = 0.1)(M)
     M = Flatten()(M)
     
-    M = Dense(128, activation = None, use_bias = False, kernel_initializer = 'glorot_normal')(M) 
-    
-    Z_L = ArcFaceLossLayer(class_num = NUM_LABELS)([M, label])
+    M = Dense(128, activation = None, use_bias = False)(M) 
+
+    Z_L = ArcFaceLossLayer(class_num = OLD_NUM_LABELS)([M, label])
     
     model = Model(inputs = [X, label], outputs = Z_L, name = 'mobile_face_net')
     
@@ -148,15 +165,44 @@ def mobile_face_net():
 
 model = mobile_face_net()
 
+# model.summary()
+# model.layers
+
+'''Loading the model & re-defining'''
+print("Reading the pre-trained model") 
+model.load_weights(r'../Models/MobileFaceNet_train.h5')
+# model.load_weights("E:\\Python_Coding\\MobileFaceNet\\tl_model_1905270955.hdf5")
+print("Reading done. ")
 model.summary()
-model.layers
+# model.layers
 
-'''Training the Model'''
-# Train on multiple GPUs
-# from keras.utils import multi_gpu_model
-# model = multi_gpu_model(model, gpus = 2)
+# Re-define the model
+model.layers.pop() # Remove the ArcFace Loss Layer
+model.layers.pop() # Remove the Label Input Layer
+model.summary()
 
-model.compile(optimizer = Adam(lr = 0.001, epsilon = 1e-8), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+model.layers[-1].outbound_nodes = []
+model.outputs = [model.layers[-1].output] # Reset the output
+output = model.get_layer(model.layers[-1].name).output
+model.input
+# The model used for prediction
+pred_model = Model(model.input[0], output)
+pred_model.summary()
+# pred_model.save('pred_model.h5')
+
+# Custom the model for continue training
+label = Input((NUM_LABELS, ))
+M = pred_model.output
+Z_L = ArcFaceLossLayer(class_num = NUM_LABELS)([M, label])
+
+customed_model = Model(inputs = [pred_model.input, label], outputs = Z_L, name = 'mobile_face_net_transfered')
+customed_model.summary()
+# customed_model.layers
+# plot_model(customed_model, to_file='customed_model.png')
+
+'''Setting configurations for training the Model''' 
+customed_model.compile(optimizer = Adam(lr = 0.01, epsilon = 1e-8), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+# Temporarily increase the learing rate to 0.01
 
 # Save the model after every epoch
 from keras.callbacks import ModelCheckpoint 
@@ -164,7 +210,7 @@ check_pointer = ModelCheckpoint(filepath = '../Models/MobileFaceNet_train.h5', v
 
 # Interrupt the training when the validation loss is not decreasing
 from keras.callbacks import EarlyStopping
-early_stopping = EarlyStopping(monitor = 'val_loss', patience = 10000)
+early_stopping = EarlyStopping(monitor = 'val_loss', patience = 1000)
 
 # Record the loss history
 class LossHistory(keras.callbacks.Callback):
@@ -184,14 +230,17 @@ csv_logger = CSVLogger('training.csv', separator = ',', append = True)
 
 # Reduce learning rate when a metric has stopped improving
 from keras.callbacks import ReduceLROnPlateau
-reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.2, patience = 200, min_lr = 0)
+reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.2, patience = 20, min_lr = 0)
 
-hist = model.fit_generator(
+'''Importing the data & training the model'''
+hist = customed_model.fit_generator(
         train_generator,
-        steps_per_epoch = 1000, # (m * (1 - DATA_SPLIT)) // BATCH_SIZE
-        epochs = 1000,
-        callbacks = [check_pointer, early_stopping, history, csv_logger, reduce_lr],
+        steps_per_epoch = 2000, # (m * (1 - DATA_SPLIT)) // BATCH_SIZE,
+        epochs = TOTAL_EPOCHS,
+        callbacks = [check_pointer, early_stopping, history, csv_logger, reduce_lr], 
         validation_data = validate_generator, 
-        validation_steps = (m * DATA_SPLIT) // BATCH_SIZE)
+        validation_steps = (m * DATA_SPLIT) // BATCH_SIZE, 
+        workers = 4, 
+        use_multiprocessing = True, 
+        initial_epoch = 9)
 
-print(hist.history)
